53ea7ac42e627b8a3fe08673c26c4727a1031f19
srividhyaravi <40314454+srividhyaravi@users.noreply.github.com>
2019-07-04 18:11:46 +0530

    Update AgeException.java

--- Dept/ISE/AgeException.java
+++ Dept/ISE/AgeException.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package college;
+package ISE;
 
 public class AgeException extends Exception
 {
